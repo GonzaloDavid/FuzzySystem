0cc35ddb0c6864e5abe6616c643fdcc486a51177
Buhobit <byron.lopez.begins@gmail.com>
2019-07-30 16:18:47 -0500

    Delphicalculations added!

--- Backend/MethodDelphi/MethodFuzzyDelphiWS/src/main/java/com/epn/fd/WS/ApplicationConfig.java
+++ Backend/MethodDelphi/MethodFuzzyDelphiWS/src/main/java/com/epn/fd/WS/ApplicationConfig.java
@@ -35,6 +35,7 @@ public class ApplicationConfig extends Application {
         resources.add(com.epn.exception.NotFoundExceptionMapper.class);
         resources.add(com.epn.fd.WS.CatalogueFacadeREST.class);
         resources.add(com.epn.fd.WS.CatalogueitemFacadeREST.class);
+        resources.add(com.epn.fd.WS.DelphicalculationsFacadeREST.class);
         resources.add(com.epn.fd.WS.MenuFacadeREST.class);
         resources.add(com.epn.fd.WS.NewCrossOriginResourceSharingFilter.class);
         resources.add(com.epn.fd.WS.PersonFacadeREST.class);
